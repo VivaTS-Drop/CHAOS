27b31451dcae8b2ccb6acc9301540961c93147b9
VivaTS-Drop <tseytlinvova@gmail.com>
2025-08-10 11:14:48 +0300

    Add files via upload

---
+++ src/ui/logo.go
@@ -0,0 +1,21 @@
+package ui
+
+import (
+	"fmt"
+
+	c "github.com/tiagorlampert/CHAOS/src/color"
+)
+
+func ShowLogo() {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println(c.RED, "▄████████    ▄█    █▄       ▄████████  ▄██████▄     ▄████████  ")
+	fmt.Println(c.RED, "███    ███   ███    ███     ███    ███ ███    ███   ███    ███ ")
+	fmt.Println(c.RED, "███    █▀    ███    ███     ███    ███ ███    ███   ███    █▀  ")
+	fmt.Println(c.RED, "███         ▄███▄▄▄▄███▄▄   ███    ███ ███    ███   ███        ")
+	fmt.Println(c.RED, "███        ▀▀███▀▀▀▀███▀  ▀███████████ ███    ███ ▀███████████ ")
+	fmt.Println(c.RED, "███    █▄    ███    ███     ███    ███ ███    ███          ███ ")
+	fmt.Println(c.RED, "███    ███   ███    ███     ███    ███ ███    ███    ▄█    ███ ")
+	fmt.Println(c.RED, "████████▀    ███    █▀      ███    █▀   ▀██████▀   ▄████████▀  ")
+	fmt.Println("")
+}
